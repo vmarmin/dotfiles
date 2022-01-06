9b920ef549f28ee9e9062b286cb68898cfd2815d
Valentin Marmin <valentin.marmin@gmail.com>
2022-01-05 21:33:37 -0500

    [#2](nvim): adding missing configuration for tree-sitter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,11 @@
 -- `init.lua` file requiring configuration files
+-- base
 require "vmarmin.options"       -- lua/vmarmin/options.lua
 require "vmarmin.keymaps"       -- lua/vmarmin/keymaps.lua
 require "vmarmin.plugins"       -- lua/vmarmin/plugins.lua
 require "vmarmin.colorscheme"   -- lua/vmarmin/colorscheme.lua
+-- plugins
+require "vmarmin.treesitter"    -- lua/vmarmin/treesitter.lua
 require "vmarmin.nvimtree"      -- lua/vmarmin/nvimtree.lua
 require "vmarmin.signify"       -- lua/vmarmin/signify.lua
 require "vmarmin.lualine"       -- lua/vmarmin/lualine.lua
